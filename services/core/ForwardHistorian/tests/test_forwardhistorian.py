0345fd4921e01918c190f630a471828101e30f84
Mike Roup <michael.roup@pnnl.gov>
2016-06-20 15:56:34 -0700

    remove actuator reference in forwarder test

--- services/core/ForwardHistorian/tests/test_forwardhistorian.py
+++ services/core/ForwardHistorian/tests/test_forwardhistorian.py
@@ -625,15 +625,6 @@ def test_actuator_topic(publish_agent, query_agent):
     print("agent id: ", master_uuid)
     gevent.sleep(2)  # wait for the agent to start and start the devices
 
-    # Start the actuator agent through which publish agent should communicate
-    # to fake device. Start the master driver agent which would intern start
-    # the fake driver using the configs created above
-    actuator_uuid = volttron_instance1.install_agent(
-        agent_dir="services/core/ActuatorAgent",
-        config_file="services/core/ActuatorAgent/tests/actuator.config",
-        start=True)
-    print("agent id: ", actuator_uuid)
-
     listener_uuid = volttron_instance2.install_agent(
         agent_dir="examples/ListenerAgent",
         config_file="examples/ListenerAgent/config",
